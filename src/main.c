6209864a827a7a563447b47822f638367ddf7c77
jusjmkim <jusjmkim@users.noreply.github.com>
2014-09-13 02:50:34 -0400

    Enter and set password texts should work

--- src/main.c
+++ src/main.c
@@ -1,13 +1,30 @@
 #include <pebble.h>
   
 static Window *s_main_window;
+static TextLayer *s_first_page_layer;
   
+static void create_enter_password() {
+  s_first_page_layer = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_background_color(s_first_page_layer, GColorClear);
+  text_layer_set_text_color(s_first_page_layer, GColorBlack);
+  text_layer_set_text(s_first_page_layer, "Enter Password");
+}
+
+static void create_set_password() {
+  s_first_page_layer = text_layer_create(GRect(1, 55, 144, 50));
+  text_layer_set_background_color(s_first_page_layer, GColorClear);
+  text_layer_set_text_color(s_first_page_layer, GColorBlack);
+  text_layer_set_text(s_first_page_layer, "Set New Password");
+}
+
 static void main_window_load(Window *window) {
-  
+  // Create first_page TextLayer
+  create_enter_password();
+  create_set_password();
 }
 
 static void main_window_unload(Window *window) {
-  
+
 }
 
 static void init() {
