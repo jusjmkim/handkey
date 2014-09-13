57293122699137339735af4a886c2be2db9b05e1
jusjmkim <jusjmkim@users.noreply.github.com>
2014-09-13 01:48:51 -0400

    More boilerplate stuff

--- src/main.c
+++ src/main.c
@@ -1,13 +1,35 @@
 #include <pebble.h>
   
-static void init() {
+static window *s_main_window;
   
+static void main_window_load(window *window) {
+  
+}
+
+static void main_window_unload(window *window) {
+  
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+  
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
 }
 
 static void deinit() {
-  
+  window_destroy(s_main_window);
 }
 
 int main(void) {
-  
+  init();
+  app_event_loop();
+  deinit();
 }
