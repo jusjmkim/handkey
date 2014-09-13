de5d9056b790f67ab2cc9f8aaf18e13033102105
jusjmkim <jusjmkim@users.noreply.github.com>
2014-09-13 08:15:44 -0400

    Code is a bit DRYer

--- src/main.c
+++ src/main.c
@@ -3,24 +3,31 @@
 static Window *s_main_window;
 static TextLayer *s_first_page_layer;
   
-static void create_enter_password() {
-  s_first_page_layer = text_layer_create(GRect(0, 55, 144, 50));
+static void set_first_page_colors() {
   text_layer_set_background_color(s_first_page_layer, GColorClear);
   text_layer_set_text_color(s_first_page_layer, GColorBlack);
+}
+
+static void create_enter_password() {
+  s_first_page_layer = text_layer_create(GRect(0, 55, 144, 50));
+  set_first_page_colors();
   text_layer_set_text(s_first_page_layer, "Enter Password");
 }
 
 static void create_set_password() {
   s_first_page_layer = text_layer_create(GRect(1, 55, 144, 50));
-  text_layer_set_background_color(s_first_page_layer, GColorClear);
-  text_layer_set_text_color(s_first_page_layer, GColorBlack);
+  set_first_page_colors();
   text_layer_set_text(s_first_page_layer, "Set New Password");
+}
+
+static void create_first_page() {
+  create_enter_password();
+  create_set_password();
 }
 
 static void main_window_load(Window *window) {
   // Create first_page TextLayer
-  create_enter_password();
-  create_set_password();
+  create_first_page();
 }
 
 static void main_window_unload(Window *window) {
