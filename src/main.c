39b3da4c12d41dae57df10055cee899e52ebcc69
jusjmkim <jusjmkim@users.noreply.github.com>
2014-09-13 10:13:58 -0400

    Centers text. still crashes

--- src/main.c
+++ src/main.c
@@ -12,27 +12,29 @@ static void set_first_page_colors() {
 
 static void create_enter_password() {
   s_enter_password_layer = text_layer_create(GRect(0, 55, 144, 50));
+  text_layer_set_text_alignment(s_enter_password_layer, GTextAlignmentCenter);
   text_layer_set_text(s_enter_password_layer, "Enter Password");
 }
 
 static void create_set_password() {
   s_set_password_layer = text_layer_create(GRect(1, 55, 144, 50));
+  text_layer_set_text_alignment(s_set_password_layer, GTextAlignmentCenter);
   text_layer_set_text(s_set_password_layer, "Set New Password");
 }
 
-static void create_first_page(Window *window) {
+static void create_first_page() {
   set_first_page_colors();
   create_enter_password();
   create_set_password();
-  
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_first_page_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_enter_password_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_set_password_layer));
 }
 
 static void main_window_load(Window *window) {
   // Create first_page TextLayer
-  create_first_page(window);
+  create_first_page();
+  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_first_page_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_enter_password_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_set_password_layer));
 }
 
 static void main_window_unload(Window *window) {
